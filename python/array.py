ca8d248ec4241ffee0e15c839a2bb1256b5b0f70
athul-mindtickle <athul.r@mindtickle.com>
2020-05-19 23:38:35 +0530

    Added ArrayImplemenation With Dictionary in Python

---
+++ python/array.py
@@ -0,0 +1,47 @@
+"""
+
+This file has the array implemention using Python Class. 
+
+"""
+
+class ArrayWithDict(object):
+	"""
+	This is the array implementation with data as the Dict Object
+
+	"""
+	def __init__(self, arg):
+		self.data = {}
+		self.length = 0
+
+	def get(self, index):
+		return self.data.get(index)
+
+	def push(self, item):
+		self.data[self.length] = item
+		self.length += 1
+		return self.length
+
+	def pop(self):
+		last_item = self.data[self.length-1]
+		del self.data[self.length-1]
+		self.length -= 1
+		return last_item
+
+	def delete(self, index):
+		item = self.data[index]
+		self.shift_items(index)
+		return item
+
+
+
+	def shift_items(self, index):
+		for i in range(index, self.length):
+			self.data[i] = self.data[i+1]
+			
+					del self.data[self.length-1]
+		self.length -= 1
+
+
+
+
+
