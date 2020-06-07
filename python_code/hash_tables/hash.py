23ca526fca87d4c6c03c1be6478087ee9e2558df
athul-mindtickle <athul.r@mindtickle.com>
2020-06-07 14:12:56 +0530

    Update: Collisions handled

--- python_code/hash_tables/hash.py
+++ python_code/hash_tables/hash.py
@@ -3,7 +3,7 @@ class Hash(object):
 	"""Hash tables using Class"""
 	
 	def __init__(self, size):
-		self.data = list()
+		self.data = [-1]*size
 		self.size = size
 
 	def __hash(key):
@@ -16,12 +16,27 @@ class Hash(object):
 
   	def set(key, value):
   		address = self.__hash(key)
-  		self.data[address] = value
+  		if self.data[address] == -1:
+			self.data[address] = [(key, value)]
+		else
+			self.data[addres].append((key, value)) 
+
+		print(self.data)
   		return None
 
 	def get(key):
   		address = self.__hash(key)
-  		return self.data[address]
+
+  		current_bucket = self.data[address]
+  		if current_bucket == -1:
+  			raise KeyError("Key Not Found")
+
+		for item in current_bucket:
+			if item[0] == key
+				return item[1]
+
+
+  		raise KeyError("Key Not Found") 
 
 
 
